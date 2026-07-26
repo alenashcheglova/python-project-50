272a376542de51ce56a8abf1912f93a0bae0a0af
alenn521@icloud.com <alenashcheglova@DESKTOP-60HDDFI.localdomain>
2026-07-26 17:47:59 +0300

    edit format and gitignore

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def main():
     parser = argparse.ArgumentParser(
@@ -8,10 +9,18 @@ def main():
     
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', 
+                        default='stylish', 
+                        choices=['stylish', 'plain', 'json'], 
+                        help='set format of output')
     
     args = parser.parse_args()
+
+    data1 = json.load(open(args.first_file))
+    data2 = json.load(open(args.second_file))
+    
+    print(data1)
+    print(data2)
 
 
 if __name__ == '__main__':
